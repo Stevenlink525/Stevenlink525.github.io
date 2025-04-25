3bbd523b2e7bc2ddccc3dc2f50b47e2e6e5eaff0
林敬傑 <Stevenlink525@gmail.com>
2025-04-25 11:23:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.querySelectorAll('.accordion h2').forEach((header) => {
+  header.addEventListener('click', () => {
+    const content = header.nextElementSibling;
+    content.style.display = (content.style.display === 'block') ? 'none' : 'block';
+  });
+});
